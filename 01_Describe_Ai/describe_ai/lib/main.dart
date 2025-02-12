4158bd23a4fb9e8429308048f6e6c063f98b3b3a
Aqsam <maqsam1155@gmail.com>
2025-02-12 23:45:41 +0500

    new changes

--- 01_Describe_Ai/describe_ai/lib/main.dart
+++ 01_Describe_Ai/describe_ai/lib/main.dart
@@ -1,21 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:describe_ai/Home_Page.dart';
+import 'package:describe_ai/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home_Page(),
-    );
-  }
-}
+// * Built using Flutter with ❤️ by Aqsam.
+// ! Give Your Feedback
+// ? Got any question
+// Todo: Let's Connect

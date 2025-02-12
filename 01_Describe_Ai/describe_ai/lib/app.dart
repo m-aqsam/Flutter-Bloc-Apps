9fa44430bac262643eda7aeb0f074fd5af0dbac6
Aqsam <maqsam1155@gmail.com>
2025-02-12 23:45:16 +0500

    app.dart

---
+++ 01_Describe_Ai/describe_ai/lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:describe_ai/Feature/Home/Ui/Home_Page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home_Page(),
+    );
+  }
+}

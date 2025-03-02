530eaa4e5c9869ebdbc563e10cdcad5f368efc1b
Aqsam <maqsam1155@gmail.com>
2025-03-02 23:23:22 +0500

    Update

--- 01_Describe_Ai/describe_ai/lib/Feature/Home/Ui/Home_Page.dart
+++ 01_Describe_Ai/describe_ai/lib/Feature/Home/Ui/Home_Page.dart
@@ -116,7 +116,6 @@ class _Home_PageState extends State<Home_Page> {
                           ),
                         ),
                 ),
-                //
               ),
             ],
           );
